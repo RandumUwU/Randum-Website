f3fbd1a8aba2d5488edd6487f5464ff619b9d453
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-05-28 21:05:40 +0200

    feat(generate-gallery): Add generate-gallery script

---
+++ generate-gallery.js
@@ -0,0 +1,20 @@
+const fs = require("node:fs");
+const path = require("path");
+
+const imagesDir = path.join(__dirname, 'images');
+const imagesOutputFile = path.join(__dirname, 'images.json');
+
+const validExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+
+try {
+    const files = fs.readdirSync(imagesDir);
+    const images = files.filter(file => {
+        const fileExtension = path.extname(file).toLowerCase();
+        return validExtensions.includes(fileExtension);
+    });
+
+    fs.writeFileSync(imagesOutputFile, JSON.stringify(images, null, 2));
+    console.log(`✅ images.json with ${images.length} images`);
+} catch (error) {
+    console.error("❌ Error reading the directory:", error);
+}
